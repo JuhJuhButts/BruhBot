9c59e4d71e16805f1528fe8753467a2de2066e20
Joel N. Evans <joelevans@outlook.com>
2020-06-16 07:35:26 -0400

    Added script to BruhBot
    Also compartmentalised config keys/values

--- BruhBot.js
+++ BruhBot.js
@@ -1,1 +1,15 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const config = require('./config.json');
 
+bot.once('ready', () => {
+	console.log('Login successful.');
+});
+
+client.on('message', message => {
+	if (message.content === '!ping') {
+		message.channel.send('Pong.');
+	}
+});
+
+bot.login(config.token);
